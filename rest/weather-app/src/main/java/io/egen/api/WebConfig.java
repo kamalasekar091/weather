794d2356f18bfa17c8231afe67c516451266a7ae
kamalasekar091 <kamalasekar091@gmail.com>
2017-06-13 12:07:20 -0500

    feat(module-controller): add ApiOperation in contoller

--- rest/weather-app/src/main/java/io/egen/api/WebConfig.java
+++ rest/weather-app/src/main/java/io/egen/api/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
@@ -23,4 +24,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**");
 	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
 }
--- rest/weather-app/src/main/java/io/egen/api/controller/WeatherController.java
+++ rest/weather-app/src/main/java/io/egen/api/controller/WeatherController.java
@@ -15,33 +15,40 @@ import org.springframework.web.bind.annotation.RestController;
 import io.egen.api.constants.URI;
 import io.egen.api.entity.Weather;
 import io.egen.api.service.WeatherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping(value = URI.WEATHER)
-
+@Api(tags="weather")
 public class WeatherController {
 
 	@Autowired
 	private WeatherService weatherservice;
 
 	@RequestMapping(method = RequestMethod.POST)
+	@ApiOperation(value="Saves Data to backend", notes="Consumes posted data from the mocker and stores in database")
 	public Weather create(@RequestBody Weather weather) {
 		return weatherservice.create(weather);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = URI.CITIES)
+	@ApiOperation(value="Find Cities", notes="Retrive the list of cities which have repoted wether in past")
 	public List<String> findCity() {
 		return weatherservice.findCity();
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = URI.RECENT_WEATHER_CITY)
+	@ApiOperation(value="Recent Weather For City", notes="Find the Recent weather of teh provided city")
 	public Weather findRecentWeatherForCity(@PathVariable("city") String city) {
 		return weatherservice.findRecentWeatherForCity(city);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = URI.RECENT_WEATHER_PROPERTY)
+	@ApiOperation(value="Find Recent Weather Property", notes="Find the Property in a recent weather reported by the city")
 	public String findRecentWeatherPropertyForCity(@PathVariable("city") String city,
 			@PathVariable("property") String property) {
 
@@ -51,11 +58,13 @@ public class WeatherController {
 
 	// findAverageWeatherForCity
 	@RequestMapping(method = RequestMethod.GET, value = URI.AVERAGE_WEATHER_PER_HOUR)
-	public List<Weather> findAverageWeatherForCity(@PathVariable("city") String city) {
+	@ApiOperation(value="Find Houry Average weather", notes="Retrive hourly average weather for the given city")
+	public List<Weather> findHourlyAverageWeatherForCity(@PathVariable("city") String city) {
 		return weatherservice.findAverageWeatherForCity(city);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = URI.AVERAGE_WEATHER_PER_DAY)
+	@ApiOperation(value="Find Daily  Average weather", notes="Retrive daily average weather for the given city")
 	public List<Weather> findDailyAverageWeatherForCity(@PathVariable("city") String city) {
 		return weatherservice.findDailyAverageWeatherForCity(city);
 	}
